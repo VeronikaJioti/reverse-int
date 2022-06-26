550c5472d97c0b4d7fdc17e1d04fd03716c57b68
VeronikaJioti <v.jioeva@gmail.com>
2022-06-26 17:00:20 +0400

    feat: funcition body create

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+ let number=n;
+ if (n<0){
+    number=n * (-1);
+ }
+ let str=number.toString();
+ let array=str.split('');
+ let reverseArray=array.reverse();
+ let result=reverseArray.join('');
+ return reslut=Number(result);
 }
